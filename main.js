c278f1036221e57ec695a5bc39dc6e60dd375de9
Ben-Kilinski <ben.kilinski@gmail.com>
2023-08-31 16:29:08 +0300

    handler events

---
+++ main.js
@@ -0,0 +1,37 @@
+//task 1
+// function t1() {
+//     alert("Welcome")
+// }
+
+
+//task 2
+// function hour(){
+//     console.log(new Date().getHours())
+// }
+
+
+//task 3
+// function t3(){
+//     myH3.style.color = "purple"
+// }
+
+//task 4
+// function t4(){
+//     p.style = "width: 200px"
+// }
+
+//task 5
+
+
+//class exercises about EVENTS 'handler'
+// function printColor(color) {
+//     console.log(color)
+// }
+
+// function printText(string) {
+//     console.log(string)
+// }
+
+// function changeH1() {
+//     myH1.innerHTML = myInput.value
+// }
